70b52854131520cbc83b5d1d949e7746e21d2db9
Unknown <ankghoro@gmail.com>
2018-07-23 22:53:07 +0700

    Update Profile

--- application/modules/api/controllers/Profile.php
+++ application/modules/api/controllers/Profile.php
@@ -44,4 +44,43 @@ class Profile extends CI_Controller
 		echo json_encode($arr_data);
 	}
 	
+	public function update_user()
+	{
+		$arr_data = array();
+		$get_param = $this->input->post();
+		if(count($get_param) > 0)
+		{
+			$data = array(
+				'nama'			=> $get_param['fullname'],
+				'type_identitas'=> $get_param['identity'],
+				'no_identitas'	=> $get_param['noidentity'],
+				'alamat'		=> $get_param['address'],
+				'tlp'			=> $get_param['phone'],
+				'email'			=> $get_param['email'],
+				'pekerjaan'		=> $get_param['pekerjaan']
+			);
+			$this->db->where('id_pengguna', $get_param['id']);
+			$update = $this->db->update('ppid_pengguna', $data);
+			
+			if(@$update)
+			{
+				$arr_data['msg'] = 'Data berhasil di ubah';
+				$arr_data['data'] = array();
+			}
+			else
+			{
+				$arr_data['msg'] = 'Data tidak berhasil di ubah';
+				$arr_data['data'] = array();
+			}
+		}
+		else
+		{
+			$arr_data['msg'] = 'Parameter Tidak Terkirim';
+			$arr_data['data'] = array();
+		}
+		
+		// print response
+		echo json_encode($arr_data);
+	}
+	
 }
